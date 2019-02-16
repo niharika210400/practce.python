89ae8b5554d75d8eeebda36c2808d84ac63df0f4
niharika <44729354+niharika210400@users.noreply.github.com>
2019-02-16 23:15:34 +0530

    Add files via upload

---
+++ prac2.py
@@ -0,0 +1,13 @@
+print("Enter number")
+n=int(input())
+def func(n):
+   fac=1
+   if n== 0:
+      print(1)
+   else:
+      while n>=1:
+        fac = fac*n
+        n = n-1
+      print(fac)
+
+func(n)
